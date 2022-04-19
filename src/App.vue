f2286879afdb543978540e7b76fd137056ba8422
VerkunderWP <s.kapustin@webpractik.ru>
2022-04-19 14:06:05 +0300

    add vue-counter

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <span
+      class="badge m-2"
+      :class="{
+        'badge-warning': count == 0,
+        'badge-primary': count > 0,
+      }"
+    >
+      {{ count === 0 ? "Zero" : count }}
+    </span>
+    <button class="btn btn-secondary btn-sm" @click="count++">Increment</button>
+  </div>
+</template>
+
+<style></style>
